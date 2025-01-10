4faa3fd3fb82d9525d795224e9d74b8a81acc324
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2025-01-10 22:08:21 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { mkdtempSync, rmSync } from 'node:fs'
+import { zValidator } from '@hono/zod-validator'
 import { $ } from 'bun'
 import { createMiddleware } from 'hono/factory'
 import { logger } from 'hono/logger'
@@ -29,74 +30,68 @@ const tempDirMiddleware = createMiddleware<{ Variables: { out: string } }>(
   },
 )
 
-app.post('/', tempDirMiddleware, async c => {
-  const body = await c.req.json()
-  const result = z
-    .object({
-      formula: z.string().min(1),
-      bussproofs: z.boolean(),
-      ebproof: z.boolean(),
-      timeout: z.number().min(0).max(10),
+app.post(
+  '/',
+  zValidator(
+    'form',
+    z.object({
+      formula: z.string().nonempty(),
+      format: z.array(z.enum(['bussproofs', 'ebproof'])).optional(),
+      timeout: z.enum(['3', '5', '10']),
+    }),
+  ),
+  tempDirMiddleware,
+  async c => {
+    const { formula, format, timeout } = c.req.valid('form')
+    const out = c.get('out')
+    // run prover
+    console.info('Proving...')
+    const { stdout, stderr, exitCode } =
+      await $`timeout ${timeout} java -jar -Xmx500m prover.jar ${formula} ${out} --format=${format?.join(',')}`
+        .nothrow()
+        .quiet()
+    // parse error
+    if (stdout.includes('Parse Error')) {
+      console.error('Failed: Parse Error')
+      return c.text(`${stdout}`)
+    }
+    // timeout
+    if (exitCode === 124) {
+      console.error('Failed: Timeout')
+      return c.text(`${stdout}Failed: Timeout`)
+    }
+    // OutOfMemoryError
+    if (stderr.includes('OutOfMemoryError')) {
+      console.error('Failed: OutOfMemoryError')
+      return c.text(`${stdout}Failed: OutOfMemoryError`)
+    }
+    // StackOverflowError
+    if (stderr.includes('StackOverflowError')) {
+      console.error('Failed: StackOverflowError')
+      return c.text(`${stdout}Failed: StackOverflowError`)
+    }
+    // Unexpected error
+    if (
+      (format?.includes('bussproofs') &&
+        !(await Bun.file(`${out}/out-bussproofs.tex`).exists())) ||
+      (format?.includes('ebproof') &&
+        !(await Bun.file(`${out}/out-ebproof.tex`).exists()))
+    ) {
+      console.error('Failed: Unexpected error')
+      console.info(`${stderr}`)
+      return c.text(`${stdout}Failed: Unexpected error`)
+    }
+    console.info('Done!')
+    return c.json({
+      text: `${stdout}`,
+      bussproofs: format?.includes('bussproofs')
+        ? await Bun.file(`${out}/out-bussproofs.tex`).text()
+        : undefined,
+      ebproof: format?.includes('ebproof')
+        ? await Bun.file(`${out}/out-ebproof.tex`).text()
+        : undefined,
     })
-    .safeParse(body)
-  if (!result.success) {
-    console.error(`Invalid Request: ${result.error}`)
-    return c.text('Invalid Request', 400)
-  }
-  const { formula, bussproofs, ebproof, timeout } = result.data
-  const format: string[] = []
-  if (bussproofs) {
-    format.push('bussproofs')
-  }
-  if (ebproof) {
-    format.push('ebproof')
-  }
-  const out = c.get('out')
-  // run prover
-  console.info('Proving...')
-  const { stdout, stderr, exitCode } =
-    await $`timeout ${timeout} java -jar -Xmx500m prover.jar ${formula} ${out} --format=${format.join(',')}`
-      .nothrow()
-      .quiet()
-  // parse error
-  if (stdout.includes('Parse Error')) {
-    console.error('Failed: Parse Error')
-    return c.text(`${stdout}`)
-  }
-  // timeout
-  if (exitCode === 124) {
-    console.error('Failed: Timeout')
-    return c.text(`${stdout}Failed: Timeout`)
-  }
-  // OutOfMemoryError
-  if (stderr.includes('OutOfMemoryError')) {
-    console.error('Failed: OutOfMemoryError')
-    return c.text(`${stdout}Failed: OutOfMemoryError`)
-  }
-  // StackOverflowError
-  if (stderr.includes('StackOverflowError')) {
-    console.error('Failed: StackOverflowError')
-    return c.text(`${stdout}Failed: StackOverflowError`)
-  }
-  // Unexpected error
-  if (
-    (bussproofs && !(await Bun.file(`${out}/out-bussproofs.tex`).exists())) ||
-    (ebproof && !(await Bun.file(`${out}/out-ebproof.tex`).exists()))
-  ) {
-    console.error('Failed: Unexpected error')
-    console.info(`${stderr}`)
-    return c.text(`${stdout}Failed: Unexpected error`)
-  }
-  console.info('Done!')
-  return c.json({
-    text: `${stdout}`,
-    bussproofs: bussproofs
-      ? await Bun.file(`${out}/out-bussproofs.tex`).text()
-      : undefined,
-    ebproof: ebproof
-      ? await Bun.file(`${out}/out-ebproof.tex`).text()
-      : undefined,
-  })
-})
+  },
+)
 
 export default app
